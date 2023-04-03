b4285a49e9e7e6c050d6a8c1d5f0e9b1255f73a7
wasanRuntime <andrewwasan@gmail.com>
2023-04-03 09:13:37 -0700

    Fixed another bug

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *_memset -   fills memory with a constant byte
- *@s: pointer block of memory to fill
- *@b: value to set
- *@n: bytes of the memory
- *Return: dest
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
  */
-
 char *_memset(char *s, char b, unsigned int n)
-
 {
-	unsigned int i;
+int i = 0;
 
-	/*Delacring FOR*/
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b; /*add 1 position s*/
-
-	} /*END FOR*/
-
-	return (s);
+for (; n > 0; i++)
+{
+s[i] = b;
+n--;
+}
+return (s);
 }
