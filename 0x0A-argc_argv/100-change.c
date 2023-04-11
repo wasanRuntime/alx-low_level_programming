104722750b996762326c466220927e440195f2c3
wasanRuntime <andrewwasan@gmail.com>
2023-04-11 01:36:52 +0000

    correction

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,42 +3,42 @@
 #include "main.h"
 
 /**
-* main - prints the minimum. 
-* @argc: argument counter.
-* @argv: number of arguments passed.
-* Return: least number of coins, 0 if negative amount, 1 if amount not given.
-*/
+ *main - prints the minimum number of coins for an amount of money
+ *@argc: count two arguments
+ *@argv: arguments given (should be program name and amount of money)
+ *Return: least number of coins, 0 if negative amount, 1 if amount not given
+ */
 
 int main(int argc, char *argv[])
 {
-int n, coins = 0;
+	int n, coins = 0;
 
-/* check input */
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
+	/* check and validate input */
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-if (argv[1][0] == '-')
-{
-printf("0\n");
-return (0);
-}
+	if (argv[1][0] == '-')
+	{
+		printf("0\n");
+		return (0);
+	}
 
-/* calculate coins */
-n = atoi(argv[1]);
+	/* converting string to int and calculate coins */
+	n = atoi(argv[1]);
 
-coins += n / 25;
-n = n % 25;
-coins += n / 10;
-n = n % 10;
-coins += n / 5;
-n = n % 5;
-coins += n / 2;
-n = n % 2;
-coins += n / 1;
+	coins += n / 25;
+	n = n % 25;
+	coins += n / 10;
+	n = n % 10;
+	coins += n / 5;
+	n = n % 5;
+	coins += n / 2;
+	n = n % 2;
+	coins += n / 1;
 
-printf("%d\n", coins);
-return (0);
+	printf("%d\n", coins);
+	return (0);
 }
