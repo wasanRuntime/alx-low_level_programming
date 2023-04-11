d5985d2316e3f89b60e17a51904dc11809a9f88d
wasanRuntime <andrewwasan@gmail.com>
2023-04-11 09:52:57 -0700

    Corrections

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,29 +1,53 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _strdup - Returns a pointer to a copy of the string given as a parameter.
- * @str: String given as a parameter.
- *
- * Return: return (0)
+ * _strlen - calculates the length of a given string.
+ * @s: string to be measured
+ * Return: length of the string
  */
-
-char *_strdup(char *str)
+unsigned int _strlen(char *s)
 {
-	unsigned int i = 0, length = 0;
-	char *s = NULL;
+	unsigned int i = 0;
 
-	if (str == NULL)
-		return (NULL);
-
-	for (; *(str + length) != '\0'; length++)
+	for (; *(s + i) != '\0'; i++)
 		;
 
-	s = (char *)malloc((length + 1) * sizeof(char));
-	if (s == NULL)
+	return (i);
+}
+
+/**
+ * str_concat - Function that concatenates two strings.
+ * @s1: String s1 given as a first parameter.
+ * @s2: String s2 given as a second parameter.
+ *
+ * Return: char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int size1 = 0, size2 = 0, i = 0;
+	char *s1s2 = NULL;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+
+	s1s2 = (char *)malloc((size1 + size2 + 1) * (sizeof(char)));
+
+	if (s1s2 == NULL)
 		return (NULL);
 
-	for (; i < (length + 1); i++)
-		*(s + i) = *(str + i);
+	for (i = 0; i < size1; i++)
+		*(s1s2 + i) = *(s1 + i);
 
-	return (s);
+	for (i = 0; i < size2; i++)
+		*(s1s2 + (size1 + i)) = *(s2 + i);
+
+	*(s1s2 + (size1 + size2)) = '\0';
+
+	return ((char *)s1s2);
 }
