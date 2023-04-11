0db51098c2cff716dd3fa2840253dbdc63528f12
wasanRuntime <andrewwasan@gmail.com>
2023-04-11 10:09:16 -0700

    corrected main.h idndef

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
@@ -10,4 +10,4 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 
-#endif /* HOLBERTON_H */
+#endif
