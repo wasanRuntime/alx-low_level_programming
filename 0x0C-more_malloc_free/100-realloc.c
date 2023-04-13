087e7950a3668cc9720075c4c7bbfac8752c6083
wasanRuntime <andrewwasan@gmail.com>
2023-04-13 15:00:34 -0700

    debugging

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,48 +1,49 @@
-#include "main.h"
-#include <stdlib.h>
-#include <stddef.h>
+#include<stdlib.h>
+#include"main.h"
+
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: input pointer
- * @old_size: size of old ptr
- * @new_size: size of new ptr
- * Return: reallocated ptr
- */
+* _realloc - reallocates a memory block using malloc and free
+* @ptr: pointer to the memory previsouly allocated by malloc
+* @old_size: size of the allocated memory for ptr
+* @new_size: new size of the new memory block
+* Return: pointer to the newly allocated memory block
+*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *res = NULL;
+char *ptr1;
+char *old_ptr;
+unsigned int j;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (!ptr)
-	{
-		free(ptr);
-		res = malloc(new_size);
-		return (res);
-	}
-	if (!new_size && ptr)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	res = malloc(new_size);
-	_memcpy(res, ptr, old_size);
-	free(ptr);
-	return (res);
+if (new_size == old_size)
+return (ptr);
+
+if (new_size == 0 && ptr)
+{
+free(ptr);
+return (NULL);
 }
 
-/**
- * _memcpy - copies memory area
- * @dest: destination string
- * @src: source string
- * @n: number of bytes to be copied
- * Return: pointer to dest
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	char *ptr = dest;
+if (!ptr)
+return (malloc(new_size));
 
-	while (n--)
-		*dest++ = *src++;
-	return (ptr);
+ptr1 = malloc(new_size);
+if (!ptr1)
+return (NULL);
+
+old_ptr = ptr;
+
+if (new_size < old_size)
+{
+for (j = 0; j < new_size; j++)
+ptr1[j] = old_ptr[j];
+}
+
+if (new_size > old_size)
+{
+for (j = 0; j < old_size; j++)
+ptr1[j] = old_ptr[j];
+}
+
+free(ptr);
+return (ptr1);
 }
