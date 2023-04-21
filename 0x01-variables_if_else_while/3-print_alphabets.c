61d810c61d56415d6b30eef05a6032e5ba19982d
wasanRuntime <andrewwasan@gmail.com>
2023-04-21 02:30:11 -0700

    Hot fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,50 +1,25 @@
-#include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
-void print_all(const char * const format, ...)
+
+int main(void)
 {
-	int k = 0;
-	char *str, *sep = "";
-
-	va_list lst;
-
-	va_start(lst, format);
-
-	if (format)
-	{
-		while (format[k])
-		{
-			switch (format[k])
-			{
-				case 'c':
-					printf("%s%c", sep, va_arg(lst, int));
-					break;
-				case 'i':
-					printf("%s%d", sep, va_arg(lst, int));
-					break;
-				case 'f':
-					printf("%s%f", sep, va_arg(lst, double));
-					break;
-				case 's':
-					str = va_arg(lst, char *);
-					if (!str)
-						str = "(nil)";
-					printf("%s%s", sep, str);
-					break;
-				default:
-					k++;
-					continue;
-			}
-			sep = ", ";
-			k++;
-		}
-	}
-
-	printf("\n");
-	va_end(lst);
+int lowerCase = 'a';
+int upperCase = 'A';
+while (lowerCase <= 'z')
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+while (upperCase <= 'Z')
+{
+putchar(upperCase);
+upperCase += 1;
+}
+putchar('\n');
+return (0);
 }
