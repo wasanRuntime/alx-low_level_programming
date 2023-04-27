627856c5e9db2d31b99fb8a2e251ba12737fd573
wasanRuntime <andrewwasan@gmail.com>
2023-04-27 18:36:37 +0000

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,8 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * first - prints a string before the main
+ * 
  */
 void first(void)
 {
