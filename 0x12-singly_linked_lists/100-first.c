fb58a24603b5e87ce9cc7e7337b1f556e78c006d
wasanRuntime <86593502+wasanRuntime@users.noreply.github.com>
2023-04-27 23:04:31 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
-
+void print_fun(void) __attribute__ ((constructor));
 /**
- * first - prints a string before the main
- * 
+ * print_fun -  prints a string before the main function
  */
-void first(void)
+
+void print_fun(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
