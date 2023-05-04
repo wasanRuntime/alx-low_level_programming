b8c1d84169910ab879ea560ca051545af904fddb
wasanRuntime <andrewwasan@gmail.com>
2023-05-04 14:58:44 +0000

    prints the binary representation of a number

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,24 +2,24 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at a given index
+ * get_bit - prints the binary representation of a number
  * @index: -index of a given bit
- * @n:- unsigned long int to get bit from
- * Return: value of the bit at index or -1 if an error occurred
+ * @n:- variable to get bit from
+ * Return: value of the bit (Success) or -1 (Error occurred)
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask;
+	unsigned long int is_msk;
 
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 	return (-1);
 	}
 
-	mask = 1ul << index;
+	is_msk = 1ul << index;
 
-	if ((n & mask) == mask)
+	if ((n & is_msk) == is_msk)
 	{
 	return (1);
 	}
