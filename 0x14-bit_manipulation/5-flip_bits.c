4e4e91443231d85d16f379d9a01729836cdcdb7e
wasanRuntime <andrewwasan@gmail.com>
2023-05-04 15:12:57 +0000

    to get from one number to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 
 /**
- * flip_bits - Returns the number of bits needed to flip to get from one
- *             number to another.
- * @n: The first number.
- * @m: The second number.
+ * flip_bits - returns the number of bits you would need to flip
+ *   to get from one number to another
+ * @n: The 1st number.
+ * @m: The 2nd number.
  *
- * Return: The number of bits needed to flip to get from @n to @m.
+ * Return: The number of bits needed to flip(is_flip).
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor_result = n ^ m;
-	unsigned int num_flips = 0;
+	unsigned long int res_xor = n ^ m;
+	unsigned int is_flip = 0;
 
-	while (xor_result > 0)
+	while (res_xor > 0)
 	{
-	if ((xor_result & 1) == 1)
-	num_flips++;
-	xor_result >>= 1;
+	if ((res_xor & 1) == 1)
+	is_flip++;
+	res_xor >>= 1;
 	}
-	return (num_flips);
+	return (is_flip);
 }
 
